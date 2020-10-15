40a6175f54ad8eecd0bf9336b2246d34c9708312
USER <USER@LAPTOP-DJQE6P35>
2020-10-15 15:44:57 +0530

    first commit

---
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -0,0 +1,23 @@
+package com.lti.model;
+
+public class Customer {
+	int custId;
+	String customer;
+
+	public int getCustId() {
+		return custId;
+	}
+
+	public void setCustId(int custId) {
+		this.custId = custId;
+	}
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+
+}
